d450f51e12df38c9fe4e2a1af5fdbf85421feb5d
prameetksinha <prameet.sinha@gmail.com>
2024-12-17 10:33:18 +0530

    Update main.java
    added comment

--- src/Myfirstprog/main.java
+++ src/Myfirstprog/main.java
@@ -14,6 +14,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Hello World");
+		 // adding comment for hello world
 
 	}
 
