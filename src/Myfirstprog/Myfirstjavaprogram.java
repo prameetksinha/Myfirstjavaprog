d0a932ded601f045b03468860603623773f727e1
prameetksinha <prameet.sinha@gmail.com>
2024-12-18 10:21:59 +0530

    updated main class
    Added new file Myfirstjavaprogram

---
+++ src/Myfirstprog/Myfirstjavaprogram.java
@@ -0,0 +1,12 @@
+package Myfirstprog;
+
+public class Myfirstjavaprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Hello World");
+		 // adding comment for hello world
+
+	}
+
+}
